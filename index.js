ba93480ca66770c9829f0117e9fc8280e486ffe9
TekkosH <jLoiolaR.SWE@outlook.com>
2025-11-21 16:55:48 -0300

    meu teceiro commit + fim do projeto

--- index.js
+++ index.js
@@ -1,34 +1,25 @@
-let playername = "Nome do Jogador"
-let XP = 0
+let playername = "Kakashi"
+let XP = 10001
 let rank = "null"
 
-switch (XP){
-
-    case XP <= 1000:
-        rank = "Ferro"
-    break
-    case XP >= 1001 && XP <= 2000:
-        rank = "Bronze"
-    break
-    case XP >= 2001 && XP <= 5000:
-        rank = "Prata"
-    break
-    case XP >= 5001 && XP <= 7000:
-        rank = "Ouro"
-    break
-    case XP >= 7001 && XP <= 8000:
-        rank = "Platina"
-    break
-    case XP >= 8001 && XP <= 9000:
-        rank = "Ascendente"
-    break
-    case XP >= 9001 && XP <= 10000:
-        rank = "Imortal"
-    break
-    case XP >= 10001:
-        rank = "Radiante"
-    break
-
+if (XP <= 1000) {
+    rank = "Ferro"
+} else if (XP >= 1001 && XP <= 2000) {
+    rank = "Bronze"
+} else if (XP >= 2001 && XP <= 5000){
+    rank = "Prata"
+} else if (XP >= 5001 && XP <= 7000){
+    rank = "Ouro"
+} else if (XP >= 7001 && XP <= 8000){
+    rank = "Platina"
+} else if (XP >= 8001 && XP <= 9000){
+    rank = "Ascendente"
+} else if (XP >= 9001 && XP <= 10000){
+    rank = "Imortal"
+} else {
+    rank = "Radiante"
 }
+
+
 
 console.log("O Herói de nome " + playername + " está no nível de " + rank)
