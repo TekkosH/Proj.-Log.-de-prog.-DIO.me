20e840cca5865e24124b8aa889e832d8df9c4c9b
TekkosH <jLoiolaR.SWE@outlook.com>
2025-11-25 02:40:02 -0300

    Desafio encerrado.

--- Desafio2.js
+++ Desafio2.js
@@ -1,1 +1,25 @@
-console.log("Olá Mundo!")
+function playerRanking(wins, losses) {
+    let rankCalc = wins - losses
+    return rankCalc
+}
+
+let playerRank = playerRanking(150, 80)
+let rank = "null"
+
+if (playerRank <= 10) {
+    rank = "Ferro"
+} else if (playerRank >=11 && playerRank <= 20) {
+    rank = "Bronze"
+}else if (playerRank >= 21 && playerRank <= 50){
+    rank = "Prata"
+}else if (playerRank >= 51 && playerRank <= 80){
+    rank = "Ouro"
+}else if (playerRank >= 81 && playerRank <= 90){
+    rank = "Diamante"
+} else if (playerRank >= 91 && playerRank <= 100){
+    rank = "Lendário"
+} else {
+    rank = "Imortal"
+}
+
+console.log(`O Herói tem saldo de ${playerRank} e está no nível de ${rank}`)
